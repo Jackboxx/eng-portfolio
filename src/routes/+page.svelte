f43d687e21d1b5b08512bb727c67a68e1d4c2b79
jackboxx <lucygschwantner@gmail.com>
2023-05-01 12:31:00 +0200

    add typewriter component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,50 @@
-<div class="text-3xl font-bold text-red-600 underline">Hello world!</div>
+<script>
+	import Typewriter from '../components/typewriter.svelte';
+</script>
+
+<div class="p-4 text-3xl">
+<Typewriter>
+    <div>
+		some text to test this effect
+		<div>
+			<span class="text-red-400">stuff</span>
+            hi
+			<span class="text-green-400">more stuff</span>
+		</div>
+        more
+        <div class="font-bold">
+            <span> hello world</span>
+        </div>
+	</div>
+</Typewriter>
+
+<Typewriter typingDelayMs={100}>
+    <div>
+		some text to test this effect
+		<div>
+			<span class="text-red-400">stuff</span>
+            hi
+			<span class="text-green-400">more stuff</span>
+		</div>
+        more
+        <div class="font-bold">
+            <span> hello world</span>
+        </div>
+	</div>
+</Typewriter>
+
+<Typewriter typingDelayMs={20}>
+    <div>
+		some text to test this effect
+		<div>
+			<span class="text-red-400">stuff</span>
+            hi
+			<span class="text-green-400">more stuff</span>
+		</div>
+        more
+        <div class="font-bold">
+            <span> hello world</span>
+        </div>
+	</div>
+</Typewriter>
+</div>
