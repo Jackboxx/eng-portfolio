e804b4d2c36a62e2a7f38cc7b6db5c32670a65d5
jackboxx <lucygschwantner@gmail.com>
2023-05-12 21:10:43 +0200

    add research journal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,6 +17,46 @@
 			href: 'ted-talks',
 			position: { x: 280, y: 100 },
 		},
+		{
+			title: 'Research Journal',
+			href: 'research-journal',
+			position: { x: 310, y: 200 },
+		},
+		{
+			title: 'WIP',
+			href: 'WIP',
+			position: { x: 390, y: 250 },
+		},
+		{
+			title: 'WIP',
+			href: 'WIP',
+			position: { x: 370, y: 350 },
+		},
+		{
+			title: 'WIP',
+			href: 'WIP',
+			position: { x: 320, y: 320 },
+		},
+		{
+			title: 'WIP',
+			href: 'WIP',
+			position: { x: 350, y: 420 },
+		},
+		{
+			title: 'WIP',
+			href: 'WIP',
+			position: { x: 580, y: 520 },
+		},
+		{
+			title: 'WIP',
+			href: 'WIP',
+			position: { x: 620, y: 590 },
+		},
+		{
+			title: 'WIP',
+			href: 'WIP',
+			position: { x: 690, y: 660 },
+		},
 	];
 </script>
 
---
+++ src/routes/research-journal/+page.svelte
@@ -0,0 +1,312 @@
+<script>
+	import TypingBoard from '$lib/typing/typingBoard.svelte';
+</script>
+
+<TypingBoard>
+	<h1 id="research-journal-on-job-satisfaction">Research Journal on Job Satisfaction</h1>
+	<h2 id="journal-entries">Journal Entries</h2>
+	<h3 id="basic-inquiry">1. Basic Inquiry</h3>
+	<p>
+		It seems that job satisfaction can be split up into different types of satisfaction
+		based on perception: in this case there is cognitive, affective/emotional, and
+		behavioral parts. Job satisfaction can also be evaluated on a global level (does an
+		employee enjoy their job) and a local level (does an employee enjoy a specific part of
+		their job). &gt;&gt; 1. “Job satisfaction can be measured in cognitive (evaluative),
+		affective (or emotional), and behavioral components.” Also, when looking for surveys
+		on employee satisfaction instead of results I mainly found surveys that managers could
+		give to their employees instead of studies. I am curious to see how/if the things
+		employees want are different than what managers say they want. At the moment I am
+		concerned that modern advice on how to increase employee job satisfaction is being
+		prioritized over actions that a business could take to directly increase the material
+		well being over their employees (i.e. Raising wages). If my believe will hold up to
+		further research remains to be seen.
+	</p>
+	<h4 id="sources">Sources</h4>
+	<ol type="1">
+		<li>
+			<a href="https://en.wikipedia.org/wiki/Job_satisfaction"
+				>Job Satisfaction Wikipedia</a
+			>
+		</li>
+		<li>
+			<a href="https://positivepsychology.com/job-satisfaction-questionnaires-surveys/"
+				>Survey for Managers #1</a
+			>
+		</li>
+		<li>
+			<a
+				href="https://www.getfeedback.com/resources/employee-experience/the-20-best-employee-satisfaction-survey-questions/"
+				>Survey for Managers #2</a
+			>
+		</li>
+		<li>
+			<a href="https://www.surveymonkey.com/mp/employee-satisfaction-surveys/"
+				>Survey for Managers #3</a
+			>
+		</li>
+	</ol>
+	<h4 id="words">Words</h4>
+	<ul>
+		<li>
+			Affective: The affective domain of learning includes emotions, attitudes, and
+			values, and is just as important as the cognitive domain.
+		</li>
+		<li>
+			Cognitive: Cognitive development refers to the growth and development of mental
+			processes such as thinking, reasoning, and problem-solving.
+		</li>
+		<li>
+			Inquiry: Inquiry-based learning is a teaching method that encourages students to ask
+			questions, explore topics, and discover knowledge through active learning.
+		</li>
+		<li>
+			Global: The global economy is becoming increasingly interconnected, with businesses
+			and economies around the world influencing each other more than ever before.
+		</li>
+	</ul>
+	<h3 id="deeper-theory">2. Deeper Theory</h3>
+	<p>
+		Research into job satisfaction began in the 1930s and mainly focused on the nature of
+		the job being worked and the relation ship with coworkers and supervisors. &gt;&gt; 2.
+		“The assessment of job satisfaction through employee anonymous surveys became
+		commonplace in the 1930s. Although prior to that time there was the beginning of
+		interest in employee attitudes, there were only a handful of studies published.” In
+		the following decades several models of job satisfaction emerged.
+	</p>
+	<h4 id="affect-theory">Affect Theory:</h4>
+	<p>
+		This theory/model focuses on the dissonance with what a person has in there job and
+		what they want from their job. In addition, how much a person values something
+		determines how much they are effected when their job meats/doesn’t meats this
+		value/expectation.
+	</p>
+	<h4 id="equity-theory">Equity Theory:</h4>
+	<p>
+		This theory focuses on how a person perceives fairness in a company. In this theory a
+		person evaluates the personal output (the effort put into the company) versus the
+		input (the things gained from the company). A person also measures their output
+		against the output other people are investing for a particular input to determine if
+		the output-input ration is fair. There are other models that have been develop but
+		these were the most interesting to me. One thing that is interesting to note is that
+		the theories are not just applicable to job satisfaction but to general human
+		behavior. In the next entry I’ll be looking through some surveys and studies to see
+		how these theories match up with data.
+	</p>
+	<h4 id="sources-1">Sources</h4>
+	<ol type="1">
+		<li>
+			<a href="https://en.wikipedia.org/wiki/Job_satisfaction#Models"
+				>Job Satisfaction Models</a
+			>
+		</li>
+		<li>
+			<a href="https://en.wikipedia.org/wiki/Job_satisfaction#History"
+				>Job Satisfaction History</a
+			>
+		</li>
+	</ol>
+	<h4 id="words-1">Words</h4>
+	<ul>
+		<li>
+			Equity: In education, equity refers to the principle of ensuring that all students
+			have access to the same opportunities and resources, regardless of their background
+			or circumstances.
+		</li>
+		<li>
+			Hypothesis: A hypothesis is a proposed explanation for a phenomenon, based on
+			limited evidence, that can be tested through further investigation and
+			experimentation.
+		</li>
+		<li>
+			Model: A model is a simplified representation of a complex system or concept, often
+			used to make predictions or understand how different variables interact with each
+			other.
+		</li>
+		<li>
+			Theory: A theory is a well-substantiated explanation for a broad range of phenomena,
+			supported by a large body of evidence from multiple sources and validated through
+			further research.
+		</li>
+	</ul>
+	<h3 id="research-and-data">3. Research and Data</h3>
+	<p>
+		According to the research papers I have read so far salary seems to be the least
+		significant. &gt;&gt; 3. “The results show that worst assessed facet was (1) salary
+		and security…”. On the other hand factors like leadership and nature of the job were
+		more important[2] in increasing the enjoyment an employee got out of their work. In
+		addition, the type of work (permanent or contingent employment) seem to have a
+		measurable effect on job satisfaction[1]. However, it needs to be mentioned that some
+		contingent workers have similar job satisfaction as permanent workers. I am interested
+		to see if this might be related to the type of clients these contingent workers
+		interact with.
+	</p>
+	<h4 id="sources-2">Sources</h4>
+	<ol type="1">
+		<li>
+			<a href="https://onlinelibrary.wiley.com/doi/abs/10.1002/job.1790"
+				>I can’t get no job satisfaction: Meta-analysis comparing permanent and contingent
+				workers</a
+			>
+		</li>
+		<li>
+			<a
+				href="https://www.emerald.com/insight/content/doi/10.1108/13660750610665008/full/html"
+				>A study of relationship between managers’ leadership style and employees’ job
+				satisfaction</a
+			>
+		</li>
+		<li>
+			<a
+				href="https://web.archive.org/web/20201124123702/http://pdfs.semanticscholar.org/d667/e27d0cdf0e51cdc5ff91a9da80ded1a8e5aa.pdf"
+				>Factors Influencing Employee Satisfaction in the Police Service: The Case of
+				Slovenia</a
+			>
+		</li>
+		<li>
+			<a href="https://psycnet.apa.org/record/1985-21607-001"
+				>Job satisfaction and job performance: A meta-analysis.</a
+			>
+		</li>
+	</ol>
+	<h4 id="words-2">Words</h4>
+	<ul>
+		<li>
+			Clients: In the context of business, clients are individuals or organizations who
+			purchase goods or services from a company.
+		</li>
+		<li>
+			Contingent: A contingent workforce is a group of workers who are employed on a
+			temporary or flexible basis, often to meet specific needs or projects.
+		</li>
+		<li>
+			Meta-analysis: Meta-analysis is a statistical technique used in research to combine
+			the results of multiple studies on a particular topic, in order to generate a more
+			comprehensive and accurate understanding of the subject.
+		</li>
+		<li>
+			Factors: Factors are variables or elements that contribute to a particular outcome
+			or result, and can include a wide range of influences such as environmental, social,
+			economic, or psychological factors.
+		</li>
+	</ul>
+	<h3 id="investigating-contingent-work">4. Investigating contingent work</h3>
+	<p>
+		The literature strongly indicates that only temporary agency workers in the contingent
+		worker landscape have significantly lower job satisfaction than there permanently
+		employed peers. This dissatisfaction seems to largely be due to a lack of satisfaction
+		with the content of their jobs. &gt;&gt; 2. “Decomposition of this gap indicates that
+		the major part is due to the low satisfaction experienced by agency workers regarding
+		the content of their jobs.” In addition, a lower job security and lower average salary
+		also slightly contribute to this lower satisfaction. Many modern companies that employ
+		contingent workers often due so to outsource menial tasks that they don’t want to deal
+		with themselves which results in contingent jobs that are not satisfying to the
+		workers doing them. This leads me to believe that there might be a decrease in job
+		satisfaction in more than just the contingent sector but this requires further
+		research.
+	</p>
+	<h4 id="sources-3">Sources</h4>
+	<ol type="1">
+		<li>
+			<a href="https://openaccess.uoc.edu/bitstream/10609/92463/1/contingent.pdf"
+				>Contingent workforce, organisational commitment and job satisfaction: Review,
+				discussion and research agenda</a
+			>
+		</li>
+		<li>
+			<a href="file:///home/lucifer/downloads/s10645-011-9180-7.pdf"
+				>Job Satisfaction and Contingent Employment</a
+			>
+		</li>
+	</ol>
+	<h4 id="words-3">Words</h4>
+	<ul>
+		<li>
+			Organization: An organization is a group of individuals or entities that come
+			together to pursue a common goal or objective, and is typically structured around a
+			formal hierarchy and set of procedures.
+		</li>
+		<li>
+			Outsourcing: Outsourcing is the practice of contracting out certain business
+			functions or processes to external vendors or service providers, often in order to
+			reduce costs or increase efficiency.
+		</li>
+		<li>
+			Decomposition: Decomposition is the process of breaking down complex systems,
+			structures, or concepts into simpler, more manageable components in order to
+			facilitate understanding and analysis.
+		</li>
+		<li>
+			Peers: Peers are individuals who are similar in age, background, or experience, and
+			are often considered to be on an equal footing in terms of social status or
+			professional standing.
+		</li>
+	</ul>
+	<h3 id="job-satisfaction-trends">5. Job Satisfaction Trends</h3>
+	<p>
+		As expected the general trend of job satisfaction (in the UK and Germany) is a
+		downward trend [2]. This appears to be caused by an increase in the intensity of work
+		given to workers and the reduction of autonomy in the work place. Adding to this
+		effect is the fact that fewer people are working their desired amount of hours in a
+		job. This effect isn’t only national it also effects individual businesses, as the job
+		satisfaction inside a variety of business has decreased over the years. *It is work
+		noting that the study on national trends (2) was published in 2005 so it is possible
+		that trends have changed since then but the fact that article 1 sees a similar trend
+		present in 2017 makes this unlikely.
+	</p>
+	<h4 id="sources-4">Sources</h4>
+	<ol type="1">
+		<li>
+			<a href="https://journals.aom.org/doi/abs/10.5465/255611"
+				>Ten Year Job Satisfaction Trends in a Stable Organization</a
+			>
+		</li>
+		<li>
+			<a href="https://onlinelibrary.wiley.com/doi/abs/10.1111/j.1467-8543.2005.00362.x"
+				>An Investigation of National Trends in Job Satisfaction in Britain and Germany</a
+			>
+		</li>
+	</ol>
+	<h4 id="words-4">Words</h4>
+	<ul>
+		<li>
+			Trend: A trend refers to a general direction in which something is developing or
+			changing over time, often based on patterns or data analysis.
+		</li>
+		<li>
+			Consulting: Consulting is a professional service in which experts provide advice,
+			guidance, and support to organizations or individuals in order to help them solve
+			specific problems or achieve particular goals.
+		</li>
+	</ul>
+	<h2 id="report">Report</h2>
+	<h3 id="introduction">Introduction</h3>
+	<p>
+		This report seeks to outline and summarize the findings that we have made regarding
+		“Job Satisfaction”.
+	</p>
+	<h3 id="influential-factors">Influential Factors</h3>
+	<p>
+		The leading literature suggests that the most important factors in increasing job
+		satisfaction are the content of the job being worked and the quality of leadership.
+		Salary on the was a less significant factor.
+	</p>
+	<h3 id="permanent-vs-contingent-work">Permanent vs Contingent Work</h3>
+	<p>
+		Research only found a small difference between these two types of work. This
+		difference only applied to temporary agency workers and is attributed to the
+		unsatisfying nature of these jobs. In addition, due to the decreased job security that
+		contingent work provides there were slight deltas between job satisfaction when
+		comparing permanent and contingent work.
+	</p>
+	<h3 id="general-trends">General Trends</h3>
+	<p>
+		Over the last 30 years job satisfaction seems to have decreased both on a national
+		level and on a company level.
+	</p>
+	<h3 id="conclusion">Conclusion</h3>
+	<p>
+		New trends like an increase in contingent work don’t seem to have massively decreased
+		job satisfaction but a decrease in happiness with the nature of a job has reduced the
+		levels of satisfaction across the labor market.
+	</p>
+</TypingBoard>
